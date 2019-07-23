ce0e52ed20a33d22f12f01bdbd4317c14b04d4d2
felipe <felipeguimaraesnunes@gmail.com>
2019-07-23 12:32:30 -0300

    Thread so the UI is not blocked. Bug corrected: faces were detected twice.

--- app/src/main/java/com/example/facerecognition/MainActivity.java
+++ app/src/main/java/com/example/facerecognition/MainActivity.java
@@ -33,19 +33,35 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
     private ImageView imageView2;
     private Button button;
+    private MTCNN mtcnn;
 
     private Bitmap cropFace(Bitmap bitmap){
         Bitmap croppedBitmap = null;
         try {
-            MTCNN mtcnn = new MTCNN(getAssets());
-            Vector<Box> boxes = mtcnn.detectFaces(bitmap, 1);
+            Vector<Box> boxes = mtcnn.detectFaces(bitmap, 10);
+
+            Log.i("MTCNN", "No. of faces detected: " + boxes.size());
 
             int left = boxes.get(0).left();
             int top = boxes.get(0).top();
+
+            int x = boxes.get(0).left();
+            int y = boxes.get(0).top();
             int width = boxes.get(0).width();
             int height = boxes.get(0).height();
 
-            croppedBitmap = Bitmap.createBitmap(bitmap, top, left, width, height);
+
+            if (y + height >= bitmap.getHeight())
+                height -= (y + height) - (bitmap.getHeight() - 1);
+            if (x + width >= bitmap.getWidth())
+                width -= (x + width) - (bitmap.getWidth() - 1);
+
+            Log.i("MTCNN", "Final x: " + String.valueOf(x + width));
+            Log.i("MTCNN", "Width: " + bitmap.getWidth());
+            Log.i("MTCNN", "Final y: " + String.valueOf(y + width));
+            Log.i("MTCNN", "Height: " + bitmap.getWidth());
+
+            croppedBitmap = Bitmap.createBitmap(bitmap, x, y, width, height);
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -60,93 +76,54 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageView);
         imageView2 = findViewById(R.id.imageView2);
         button = findViewById(R.id.button);
+        mtcnn = new MTCNN(getAssets());
 
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-                photoPickerIntent.setType("image/*");
-                startActivityForResult(photoPickerIntent, PICK_IMAGEVIEW_CONTENT);
-            }
+        imageView.setOnClickListener(view -> {
+            Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+            photoPickerIntent.setType("image/*");
+            startActivityForResult(photoPickerIntent, PICK_IMAGEVIEW_CONTENT);
         });
 
-        imageView2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-                photoPickerIntent.setType("image/*");
-                startActivityForResult(photoPickerIntent, PICK_IMAGEVIEW2_CONTENT);
-            }
+        imageView2.setOnClickListener(view -> {
+            Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+            photoPickerIntent.setType("image/*");
+            startActivityForResult(photoPickerIntent, PICK_IMAGEVIEW2_CONTENT);
         });
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (image == null || image2 == null){
-                    Toast.makeText(getApplicationContext(), "One of the images haven't been set yet.", Toast.LENGTH_SHORT).show();
-                }else{
-                    //testMTCNN(image);
-
-                    FaceNet facenet = null;
+        button.setOnClickListener(view -> {
+            if (image == null || image2 == null){
+                Toast.makeText(getApplicationContext(), "One of the images haven't been set yet.", Toast.LENGTH_SHORT).show();
+            }else{
+                new Thread(() -> {
                     try {
-
-                        facenet = new FaceNet(getAssets());
-
-                        /*float[][] embeddings = facenet.runFloat(image);
-                        for (int i = 0; i < 512; i++){
-                            Log.i("embeddings", String.valueOf(embeddings[0][i]));
-                        }*/
+                        FaceNet facenet = new FaceNet(getAssets());
 
                         Bitmap face1 = cropFace(image);
                         Bitmap face2 = cropFace(image2);
 
+                        if (face1 != null)
+                            runOnUiThread(() -> imageView.setImageBitmap(face1));
+                        else
+                            Log.i("detect", "Couldn't crop image 1.");
+
+                        if (face2 != null)
+                            runOnUiThread(() -> imageView2.setImageBitmap(face2));
+                        else
+                            Log.i("detect", "Couldn't crop image 2.");
+
                         if (face1 != null && face2 != null) {
-                            double score = facenet.getSimilarityScore(cropFace(image), cropFace(image2));
+                            double score = facenet.getSimilarityScore(face1, face2);
                             Log.i("score", String.valueOf(score));
-                        }else{
-                            if (face1 == null)
-                                Log.i("score", "Couldn't crop image 1.");
-                            if (face2 == null)
-                                Log.i("score", "Couldn't crop image 2.");
+                            runOnUiThread(() -> Toast.makeText(MainActivity.this, "Similarity score: " + score, Toast.LENGTH_LONG).show());
                         }
+
                         facenet.close();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                }
+                }).start();
             }
         });
-    }
-
-    private void testMTCNN(Bitmap bitmap){
-        MTCNN mtcnn = new MTCNN(getAssets());
-        try {
-            Vector<Box> boxes = mtcnn.detectFaces(bitmap, 40);
-            Log.i("MTCNN", String.valueOf(boxes.size()));
-            for (int i = 0; i < boxes.size(); i++) {
-                Log.i("MTCNN", String.valueOf(boxes.get(i).left()));
-                Log.i("MTCNN", String.valueOf(boxes.get(i).top()));
-                Log.i("MTCNN", String.valueOf(boxes.get(i).bottom()));
-                Log.i("MTCNN", String.valueOf(boxes.get(i).right()));
-                Log.i("MTCNN", "width: " + String.valueOf(boxes.get(i).width()));
-                Log.i("MTCNN", "width: " + String.valueOf(boxes.get(i).width()));
-            }
-
-            int left = boxes.get(0).left();
-            int top = boxes.get(0).top();
-            int right = boxes.get(0).right();
-            int bottom = boxes.get(0).bottom();
-            int width = boxes.get(0).width();
-            int height = boxes.get(0).height();
-
-            Bitmap croppedBitmap = Bitmap.createBitmap(bitmap, top, left, width, height);
-
-            imageView.setImageBitmap(croppedBitmap);
-
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
     }
 
     @Override
